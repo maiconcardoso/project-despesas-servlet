702f87cd1bc937e54253d006338a640811647a10
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-08 21:54:53 -0300

    criando a primeira servlet

--- src/main/java/com/despesas/servlets/PrimeiraServlet.java
+++ src/main/java/com/despesas/servlets/PrimeiraServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class PrimeiraServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
