ee5e3188cc2de81d76dc5a2b40577993ce35d605
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-13 19:10:04 -0300

    Removido a classe anterior e recriada no pacote correto

---
+++ src/main/java/com/despesas/dao/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.despesas.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+
+public class ConnectionFactory {
+
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/despesas?useTimezone=true&serverTimezone=UTC";
+	private String username = "root";
+	private String password = "root";
+	
+	public Connection connect() throws ClassNotFoundException {
+		Connection connect = null;
+		try {
+			Class.forName(driver);
+			connect = DriverManager.getConnection(url, username, password);
+			return connect;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+			return null;
+		}
+	}
+}
