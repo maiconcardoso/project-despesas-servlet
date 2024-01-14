a13c13855a6cde47c22f0157c8de5bbdd038a6fc
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-01-14 15:48:04 -0300

    Criação da interface IDao

--- src/main/java/com/despesas/dao/DespesasDao.java
+++ src/main/java/com/despesas/dao/DespesasDao.java
@@ -2,16 +2,22 @@ package com.despesas.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import com.despesas.model.Despesas;
 
-public class DespesasDao {
+
+
+
+public class DespesasDao implements IDao<Despesas, Integer>{
 
 	private Connection connection = null;
 
+	@Override
 	public boolean create(Despesas despesas) throws ClassNotFoundException, SQLException {
 		
 		String createSQL = "INSERT INTO tb_despesas (descricao, data_, valor, categoria) " + "values (?, ?, ?, ?)";
@@ -34,16 +40,49 @@ public class DespesasDao {
 		}
 	}
 
-	public List<Despesas> readAll() {
+	public List<Despesas> readAll() throws SQLException, ClassNotFoundException {
+		
+		String readSQL = "SELECT * FROM tb_despesas";
+		List<Despesas> listaDespesas = new ArrayList<>();
+		
+		try {
+			connection = new ConnectionFactory().connect();
+			PreparedStatement pstm = connection.prepareStatement(readSQL);
+			ResultSet rs = pstm.executeQuery();
+			while (rs.next()) {
+				Despesas despesas = new Despesas();
+				despesas.setId(rs.getInt("id"));
+				despesas.setDescricao(rs.getString("descricao"));
+				despesas.setData((rs.getDate("data_").toLocalDate()));
+				despesas.setValor(rs.getDouble("valor"));
+				despesas.setCategoria(rs.getString("categoria"));
+				listaDespesas.add(despesas);
+			}
+			return listaDespesas;
+			
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());	
+			return null;
+		} finally {
+			connection.close();
+		}
+		
+	}
+	
+	@Override
+	public boolean update(Despesas obj) throws SQLException, ClassNotFoundException {
+		return false;
+	}
+
+	@Override
+	public boolean delete(Despesas obj) throws SQLException, ClassNotFoundException {
+		return false;
+	}
+
+	@Override
+	public Despesas findById(Integer id) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
 		return null;
-	}
-
-	public void update(Despesas obj) {
-
-	}
-
-	public void delete(Despesas obj) {
-
 	}
 
 }
---
+++ src/main/java/com/despesas/dao/IDao.java
@@ -0,0 +1,13 @@
+package com.despesas.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IDao<T, K> {
+	
+	public boolean create(T obj) throws ClassNotFoundException,  SQLException; 
+	public boolean update(T obj) throws ClassNotFoundException, SQLException;
+	public List<T> readAll() throws ClassNotFoundException, SQLException;
+	public boolean delete(T obj) throws ClassNotFoundException, SQLException;
+	public T findById(K id) throws ClassNotFoundException, SQLException;
+}
